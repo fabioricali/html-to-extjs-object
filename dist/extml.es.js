17e1fff9414bf85013b007df7607ff7224018041
Fabio Ricali <f.ricali@xdevel.com>
2022-10-28 10:19:38 +0200

    BUILD

--- dist/extml.es.js
+++ dist/extml.es.js
@@ -1,5 +1,8 @@
-/* Extml, version: 1.0.2 - October 27, 2022 17:28:57 */
+/* Extml, version: 1.1.0 - October 28, 2022 10:19:38 */
 var n=function(t,s,r,e){var u;s[0]=0;for(var h=1;h<s.length;h++){var p=s[h++],a=s[h]?(s[0]|=p?1:2,r[s[h++]]):s[++h];3===p?e[0]=a:4===p?e[1]=Object.assign(e[1]||{},a):5===p?(e[1]=e[1]||{})[s[++h]]=a:6===p?e[1][s[++h]]+=a+"":p?(u=t.apply(a,n(t,a,r,["",null])),e.push(u),a[0]?s[0]|=2:(s[h-2]=0,s[h]=u)):e.push(a);}return e},t=new Map;function htm(s){var r=t.get(this);return r||(r=new Map,t.set(this,r)),(r=n(this,r.get(s)||(r.set(s,r=function(n){for(var t,s,r=1,e="",u="",h=[0],p=function(n){1===r&&(n||(e=e.replace(/^\s*\n\s*|\s*\n\s*$/g,"")))?h.push(0,n,e):3===r&&(n||e)?(h.push(3,n,e),r=2):2===r&&"..."===e&&n?h.push(4,n,0):2===r&&e&&!n?h.push(5,0,!0,e):r>=5&&((e||!n&&5===r)&&(h.push(r,0,e,s),r=6),n&&(h.push(r,n,0,s),r=6)),e="";},a=0;a<n.length;a++){a&&(1===r&&p(),p(a));for(var l=0;l<n[a].length;l++)t=n[a][l],1===r?"<"===t?(p(),h=[h],r=3):e+=t:4===r?"--"===e&&">"===t?(r=1,e=""):e=t+e[0]:u?t===u?u="":e+=t:'"'===t||"'"===t?u=t:">"===t?(p(),r=1):r&&("="===t?(r=5,s=e,e=""):"/"===t&&(r<5||">"===n[a][l+1])?(p(),3===r&&(h=h[0]),r=h,(h=h[0]).push(2,0,r),r=0):" "===t||"\t"===t||"\n"===t||"\r"===t?(p(),r=2):e+=t),3===r&&"!--"===e&&(r=4,h=h[0]);}return p(),h}(s)),r),arguments,[])).length>1?r:r[0]}
+
+const STYLE_PREFIX = 'extml-style-';
+
 
 function isListener(prop) {
     return prop.startsWith('on')
@@ -9,11 +12,81 @@ function extractListenerName(prop) {
     return prop.substring(2)
 }
 
+function composeStyleInner(cssContent, tag) {
+    if (typeof cssContent !== 'string')
+        return;
+    //cssContent = mapper.getAll(cssContent);
+    let sanitizeTagForAnimation = tag.replace(/\w/g, '');
+
+    cssContent = cssContent
+        .replace(/<\/?style>/g, '')
+        .replace(/{/g, '{\n')
+        .replace(/}/g, '}\n')
+        .replace(/^(\s+)?:(component)(\s+)?{/gm, tag + ' {')
+        .replace(/:(component)/g, '')
+        .replace(/(@(?:[\w-]+-)?keyframes\s+)([\w-_]+)/g, `$1 ${sanitizeTagForAnimation}-$2`)
+        .replace(/((?:[\w-]+-)?animation(?:-name)?(?:\s+)?:(?:\s+))([\w-_]+)/g, `$1 ${sanitizeTagForAnimation}-$2`)
+        // Remove comments
+        .replace(/\/\*[\s\S]*?\*\/|([^:]|^)\/\/.*$/gm, '')
+        .replace(/\S.*{/gm, match => {
+            if (/^(@|:host|(from|to|\d+%)[^-_])/.test(match))
+                return match;
+            let part = match.split(',');
+            const sameTag = new RegExp(`^${tag.replace(/[[\]]/g, '\\$&')}(\\s+)?{`);
+            for (let i = 0; i < part.length; i++) {
+                part[i] = part[i].trim();
+                if (sameTag.test(part[i]))
+                    continue;
+                if (/^:global/.test(part[i]))
+                    part[i] = part[i].replace(':global', '');
+                else
+                    part[i] = `${tag} ${part[i]}`;
+            }
+            match = part.join(',');
+            return match;
+        });
+    cssContent = cssContent
+        .replace(/\s{2,}/g, ' ')
+        .replace(/{ /g, '{')
+        .replace(/ }/g, '}')
+        .replace(/\s:/g, ':') //remove space before pseudo classes
+        .replace(/\n/g, '')
+        .trim();
+    return cssContent;
+}
+
+function createStyle() {
+    if (!this.stylesheet) return;
+    let id = this.getId();
+    let styleElement = document.createElement('style');
+    styleElement.id = STYLE_PREFIX + id;
+    styleElement.innerHTML = composeStyleInner(this.stylesheet, '#' + id);
+    document.head.appendChild(styleElement);
+}
+
+function destroyStyle() {
+    if (!this.stylesheet) return;
+    document.getElementById(STYLE_PREFIX + this.getId()).remove();
+}
+
+function createEventObject(name, handle) {
+    let event = {};
+    event[name] = handle;
+    return event;
+}
+
+function addEvent(componentConfig, eventObject) {
+    componentConfig.listeners.push(eventObject);
+}
+
 function createComponentConfig(type, props, children, propsFunction) {
     let componentConfig = {
         xtype: type.toLowerCase(),
         items: [],
-        listeners: [],
+        listeners: [
+            createEventObject('initialize', createStyle),
+            createEventObject('destroy', destroyStyle)
+        ],
         html: ''
     };
 
@@ -21,9 +94,10 @@ function createComponentConfig(type, props, children, propsFunction) {
 
     for (let prop in props) {
         if (isListener(prop)) {
-            let event = {};
-            event[extractListenerName(prop)] = props[prop];
-            componentConfig.listeners.push(event);
+            addEvent(
+                componentConfig,
+                createEventObject(extractListenerName(prop), props[prop])
+            );
         } else {
             componentConfig[prop] = props[prop];
         }
@@ -31,14 +105,12 @@ function createComponentConfig(type, props, children, propsFunction) {
 
     children.forEach(child => {
         if (typeof child === 'string') {
-            //console.log(type, child)
             componentConfig.html += child;
         } else {
             componentConfig.items.push(child);
         }
     });
 
-    //console.log(componentConfig)
     return componentConfig
 }
 
@@ -46,9 +118,9 @@ function h(type, props, ...children) {
     if (typeof type === 'function') {
         return createComponentConfig(type.name, type(props), children, props)
     }
-    return createComponentConfig(type, props, children)
+    return createComponentConfig(type, props, children);
 }
-
+//console.dir(htm)
 const extml = htm.bind(h);
 
-export { extml as default };
+export { addEvent, composeStyleInner, createEventObject, createStyle, extml as default, destroyStyle, extractListenerName, isListener };
