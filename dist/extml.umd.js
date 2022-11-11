6014d982903476ad03ca76408d2fb68811f94c5e
Fabio Ricali <f.ricali@xdevel.com>
2022-11-11 09:59:17 +0100

    BUILD

--- dist/extml.umd.js
+++ dist/extml.umd.js
@@ -1,5 +1,5 @@
-/* Extml, version: 2.0.2 - November 10, 2022 23:43:13 */
-(function(g,f){typeof exports==='object'&&typeof module!=='undefined'?f(exports):typeof define==='function'&&define.amd?define(['exports'],f):(g=typeof globalThis!=='undefined'?globalThis:g||self,f(g.extml={}));})(this,(function(exports){'use strict';var n=function(t,s,r,e){var u;s[0]=0;for(var h=1;h<s.length;h++){var p=s[h++],a=s[h]?(s[0]|=p?1:2,r[s[h++]]):s[++h];3===p?e[0]=a:4===p?e[1]=Object.assign(e[1]||{},a):5===p?(e[1]=e[1]||{})[s[++h]]=a:6===p?e[1][s[++h]]+=a+"":p?(u=t.apply(a,n(t,a,r,["",null])),e.push(u),a[0]?s[0]|=2:(s[h-2]=0,s[h]=u)):e.push(a);}return e},t=new Map;function htm(s){var r=t.get(this);return r||(r=new Map,t.set(this,r)),(r=n(this,r.get(s)||(r.set(s,r=function(n){for(var t,s,r=1,e="",u="",h=[0],p=function(n){1===r&&(n||(e=e.replace(/^\s*\n\s*|\s*\n\s*$/g,"")))?h.push(0,n,e):3===r&&(n||e)?(h.push(3,n,e),r=2):2===r&&"..."===e&&n?h.push(4,n,0):2===r&&e&&!n?h.push(5,0,!0,e):r>=5&&((e||!n&&5===r)&&(h.push(r,0,e,s),r=6),n&&(h.push(r,n,0,s),r=6)),e="";},a=0;a<n.length;a++){a&&(1===r&&p(),p(a));for(var l=0;l<n[a].length;l++)t=n[a][l],1===r?"<"===t?(p(),h=[h],r=3):e+=t:4===r?"--"===e&&">"===t?(r=1,e=""):e=t+e[0]:u?t===u?u="":e+=t:'"'===t||"'"===t?u=t:">"===t?(p(),r=1):r&&("="===t?(r=5,s=e,e=""):"/"===t&&(r<5||">"===n[a][l+1])?(p(),3===r&&(h=h[0]),r=h,(h=h[0]).push(2,0,r),r=0):" "===t||"\t"===t||"\n"===t||"\r"===t?(p(),r=2):e+=t),3===r&&"!--"===e&&(r=4,h=h[0]);}return p(),h}(s)),r),arguments,[])).length>1?r:r[0]}const STYLE_PREFIX = 'extml-style-';
+/* Extml, version: 2.1.0 - November 11, 2022 09:59:15 */
+(function(g,f){typeof exports==='object'&&typeof module!=='undefined'?f(exports):typeof define==='function'&&define.amd?define(['exports'],f):(g=typeof globalThis!=='undefined'?globalThis:g||self,f(g.extml={}));})(this,(function(exports){'use strict';const STYLE_PREFIX = 'extml-style-';
 
 function composeStyleInner(cssContent, tag) {
     if (typeof cssContent !== 'string')
@@ -71,22 +71,13 @@ function destroyStyle() {
             this.context[this.contextName][item.getItemId()] = item;
         });
     }
-}function isEvent(prop) {
-    return prop.startsWith('on')
+}function initialize() {
+    createStyle.apply(this);
+    createContext.apply(this);
 }
 
-function extractListenerName(prop) {
-    return prop.substring(2)
-}
-
-function createEventObject(name, handle) {
-    let event = {};
-    event[name] = handle;
-    return event;
-}
-
-function addEvent(componentConfig, eventObject) {
-    componentConfig.listeners.push(eventObject);
+function destroy() {
+    destroyStyle.apply(this);
 }const htmlTags = ["a", "abbr", "acronym", "address", "applet", "area", "article", "aside", "audio", "b", "base", "basefont", "bdi", "bdo", "bgsound", "big", "blink", "blockquote", "body", "br", "button", "canvas", "caption", "center", "cite", "code", "col", "colgroup", "command", "content", "data", "datalist", "dd", "del", "details", "dfn", "dialog", "dir", "div", "dl", "dt", "element", "em", "embed", "fieldset", "figcaption", "figure", "font", "footer", "form", "frame", "frameset", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hgroup", "hr", "html", "i", "iframe", "image", "img", "input", "ins", "isindex", "kbd", "keygen", "label", "legend", "li", "link", "listing", "main", "map", "mark", "marquee", "math", "menu", "menuitem", "meta", "meter", "multicol", "nav", "nextid", "nobr", "noembed", "noframes", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "plaintext", "pre", "progress", "q", "rb", "rbc", "rp", "rt", "rtc", "ruby", "s", "samp", "script", "section", "select", "shadow", "slot", "small", "source", "spacer", "span", "strike", "strong", "style", "sub", "summary", "sup", "svg", "table", "tbody", "td", "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "tt", "u", "ul", "var", "video", "wbr", "xmp"];function defineExtClass(tag) {
     let className = 'html-' + tag;
     window.__extHtmlClass[className] = window.Ext.define(className, {
@@ -174,16 +165,23 @@ function generateHtmlClass () {
         window.__extHtmlClass = {};
         htmlTags.forEach(tag => defineExtClass(tag));
     }
-}function initialize() {
-    createStyle.apply(this);
-    createContext.apply(this);
+}var n=function(t,s,r,e){var u;s[0]=0;for(var h=1;h<s.length;h++){var p=s[h++],a=s[h]?(s[0]|=p?1:2,r[s[h++]]):s[++h];3===p?e[0]=a:4===p?e[1]=Object.assign(e[1]||{},a):5===p?(e[1]=e[1]||{})[s[++h]]=a:6===p?e[1][s[++h]]+=a+"":p?(u=t.apply(a,n(t,a,r,["",null])),e.push(u),a[0]?s[0]|=2:(s[h-2]=0,s[h]=u)):e.push(a);}return e},t=new Map;function htm(s){var r=t.get(this);return r||(r=new Map,t.set(this,r)),(r=n(this,r.get(s)||(r.set(s,r=function(n){for(var t,s,r=1,e="",u="",h=[0],p=function(n){1===r&&(n||(e=e.replace(/^\s*\n\s*|\s*\n\s*$/g,"")))?h.push(0,n,e):3===r&&(n||e)?(h.push(3,n,e),r=2):2===r&&"..."===e&&n?h.push(4,n,0):2===r&&e&&!n?h.push(5,0,!0,e):r>=5&&((e||!n&&5===r)&&(h.push(r,0,e,s),r=6),n&&(h.push(r,n,0,s),r=6)),e="";},a=0;a<n.length;a++){a&&(1===r&&p(),p(a));for(var l=0;l<n[a].length;l++)t=n[a][l],1===r?"<"===t?(p(),h=[h],r=3):e+=t:4===r?"--"===e&&">"===t?(r=1,e=""):e=t+e[0]:u?t===u?u="":e+=t:'"'===t||"'"===t?u=t:">"===t?(p(),r=1):r&&("="===t?(r=5,s=e,e=""):"/"===t&&(r<5||">"===n[a][l+1])?(p(),3===r&&(h=h[0]),r=h,(h=h[0]).push(2,0,r),r=0):" "===t||"\t"===t||"\n"===t||"\r"===t?(p(),r=2):e+=t),3===r&&"!--"===e&&(r=4,h=h[0]);}return p(),h}(s)),r),arguments,[])).length>1?r:r[0]}function isEvent(prop) {
+    return prop.startsWith('on')
 }
 
-function destroy() {
-    destroyStyle.apply(this);
+function extractListenerName(prop) {
+    return prop.substring(2)
 }
 
-function createComponentConfig(type, props, children, propsFunction) {
+function createEventObject(name, handle) {
+    let event = {};
+    event[name] = handle;
+    return event;
+}
+
+function addEvent(componentConfig, eventObject) {
+    componentConfig.listeners.push(eventObject);
+}function createComponentConfig(type, props, children, propsFunction) {
 
     // default configuration
     let componentConfig = {
@@ -208,6 +206,8 @@ function createComponentConfig(type, props, children, propsFunction) {
                 );
             } else if (prop === 'controller' && typeof configFromProps[prop] === 'function') {
                 componentConfig[prop] = configFromProps[prop]();
+            } else if (prop === 'class') {
+                componentConfig['cls'] = configFromProps[prop];
             } else {
                 componentConfig[prop] = configFromProps[prop];
             }
@@ -250,18 +250,14 @@ function createComponentConfig(type, props, children, propsFunction) {
     });
 
     return componentConfig
-}
-
-function detectClassType(xtype) {
+}function detectClassType(xtype) {
     if (xtype.startsWith('ext-')) {
         xtype = xtype.split('ext-')[1];
     } else {
         xtype = 'html-' + xtype;
     }
     return xtype;
-}
-
-function _h(type, props, ...children) {
+}function _h(type, props, ...children) {
     if (type === 'style') {
         return {isStyle: true, content: children[0] || ''}
     } else if (type === 'context') {
@@ -288,9 +284,7 @@ function h(strings, ...values) {
     }
 
     return parsed
-}
-
-try {
+}try {
     if (window) {
         generateHtmlClass();
     }
