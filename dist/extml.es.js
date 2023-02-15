95a086515ae76b2cada3052970f2a4a1a7f71a34
Fabio Ricali <f.ricali@xdevel.com>
2023-02-15 10:21:03 +0100

    BUILD

--- dist/extml.es.js
+++ dist/extml.es.js
@@ -1,4 +1,4 @@
-/* Extml, version: 2.1.4 - February 15, 2023 10:07:42 */
+/* Extml, version: 2.1.5 - February 15, 2023 10:21:01 */
 const STYLE_PREFIX = 'extml-style-';
 
 function composeStyleInner(cssContent, tag) {
@@ -73,6 +73,13 @@ function destroyStyle() {
             this.context[this.contextName][item.getItemId()] = item;
         });
     }
+}
+
+function destroyContext() {
+    Ext.getApplication().context = Ext.getApplication().context || {};
+    if (this.contextName) {
+        delete Ext.getApplication().context[this.contextName];
+    }
 }function initialize() {
     createStyle.apply(this);
     createContext.apply(this);
@@ -80,6 +87,7 @@ function destroyStyle() {
 
 function destroy() {
     destroyStyle.apply(this);
+    destroyContext.apply(this);
 }const htmlTags = ["a", "abbr", "acronym", "address", "applet", "area", "article", "aside", "audio", "b", "base", "basefont", "bdi", "bdo", "bgsound", "big", "blink", "blockquote", "body", "br", "button", "canvas", "caption", "center", "cite", "code", "col", "colgroup", "command", "content", "data", "datalist", "dd", "del", "details", "dfn", "dialog", "dir", "div", "dl", "dt", "element", "em", "embed", "fieldset", "figcaption", "figure", "font", "footer", "form", "frame", "frameset", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hgroup", "hr", "html", "i", "iframe", "image", "img", "input", "ins", "isindex", "kbd", "keygen", "label", "legend", "li", "link", "listing", "main", "map", "mark", "marquee", "math", "menu", "menuitem", "meta", "meter", "multicol", "nav", "nextid", "nobr", "noembed", "noframes", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "plaintext", "pre", "progress", "q", "rb", "rbc", "rp", "rt", "rtc", "ruby", "s", "samp", "script", "section", "select", "shadow", "slot", "small", "source", "spacer", "span", "strike", "strong", "style", "sub", "summary", "sup", "svg", "table", "tbody", "td", "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "tt", "u", "ul", "var", "video", "wbr", "xmp"];function defineExtClass(tag) {
     let className = 'html-' + tag;
     window.__extHtmlClass[className] = window.Ext.define(className, {
