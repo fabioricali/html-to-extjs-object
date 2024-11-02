bd58e8ef40ac6d056b0def7cd93c740ee8e56386
fabioricali <f.ricali@xdevel.com>
2024-11-02 14:56:06 +0100

    progress

---
+++ src/createState.js
@@ -0,0 +1,32 @@
+export function createState(initialValue) {
+    let state = initialValue;
+    const listeners = new Set();
+
+    // Funzione getter che restituisce un oggetto con valore e tipo
+    // const getState = () => ({
+    //     value: state,
+    //     $$isState: true,
+    //     subscribe,
+    // });
+    // Funzione per iscriversi ai cambiamenti di stato
+    const subscribe = (listener) => {
+        listeners.add(listener);
+        return () => listeners.delete(listener); // Ritorna una funzione per rimuovere l'observer
+    };
+    // Funzione getter che restituisce un oggetto con valore e tipo
+    const getState = () => state;
+    getState.$$isState = true; // Aggiunge la proprietà isState direttamente alla funzione
+    getState.$$subscribe = subscribe;
+
+    // Funzione setter
+    const setState = (newValue) => {
+        if (newValue !== state) {
+            state = newValue;
+            listeners.forEach((listener) => listener(state));
+        }
+    };
+
+
+
+    return [getState, setState, subscribe]; // Ritorniamo anche subscribe
+}
