e9cfd55dd4b558f628903316c2710714403c5816
fabioricali <f.ricali@xdevel.com>
2024-11-04 20:39:28 +0100

    BUILD

--- dist/extml.es.js
+++ dist/extml.es.js
@@ -1,4 +1,4 @@
-/* Extml, version: 2.2.3 - November 4, 2024 20:04:30 */
+/* Extml, version: 2.2.4 - November 4, 2024 20:39:27 */
 const STYLE_PREFIX = 'extml-style-';
 
 function composeStyleInner(cssContent, tag) {
@@ -113,7 +113,7 @@ function destroy() {
 }const htmlTags = [
     "a", "abbr", "acronym", "address", "applet", "area", "article", "aside", "audio", "b", "base", "basefont", "bdi", "bdo", "bgsound", "big", "blink", "blockquote", "body", "br", "button", "canvas", "caption", "center", "cite", "code", "col", "colgroup", "command", "content", "data", "datalist", "dd", "del", "details", "dfn", "dialog", "dir", "div", "dl", "dt", "element", "em", "embed", "fieldset", "figcaption", "figure", "font", "footer", "form", "frame", "frameset", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hgroup", "hr", "html", "i", "iframe", "image", "img", "input", "ins", "isindex", "kbd", "keygen", "label", "legend", "li", "link", "listing", "main", "map", "mark", "marquee", "math", "menu", "menuitem", "meta", "meter", "multicol", "nav", "nextid", "nobr", "noembed", "noframes", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "plaintext", "pre", "progress", "q", "rb", "rbc", "rp", "rt", "rtc", "ruby", "s", "samp", "script", "section", "select", "shadow", "slot", "small", "source", "spacer", "span", "strike", "strong", "style", "sub", "summary", "sup", "svg", "table", "tbody", "td", "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "tt", "u", "ul", "var", "video", "wbr", "xmp",
     //custom tag
-    "x-signal", "x-html"
+    "x-state", "x-html"
 ];function defineExtClass(tag) {
     let className = 'html-' + tag;
     window.__extHtmlClass[className] = window.Ext.define(className, {
@@ -329,9 +329,9 @@ function configureChildren(config, children, type) {
             addToArray(config, 'items', child);
         } else {
             if (child.$$isState) {
-                // create signal component
+                // create state component
                 addToArray(config, 'items', {
-                    xtype: 'html-x-signal',
+                    xtype: 'html-x-state',
                     html: String(child()),
                     listeners: [
                         createEventObject('initialize', (o) => {
@@ -349,6 +349,7 @@ function configureChildren(config, children, type) {
             } else {
                 let processedValue = processValueForHtml(child);
 
+                // if there is some state in the children array it's better to use the html-x-html
                 if (isPlainText(processedValue) && children.some(
                     (item) => typeof item === 'function' && item.$$isState === true
                 )) {
