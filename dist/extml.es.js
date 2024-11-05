c162f9959e6012475f8b82685edc51570e028e26
fabioricali <f.ricali@xdevel.com>
2024-11-05 17:56:39 +0100

    BUILD

--- dist/extml.es.js
+++ dist/extml.es.js
@@ -1,4 +1,4 @@
-/* Extml, version: 2.4.3 - November 5, 2024 17:56:15 */
+/* Extml, version: 2.4.4 - November 5, 2024 17:56:39 */
 const STYLE_PREFIX = 'extml-style-';
 
 function composeStyleInner(cssContent, tag) {
