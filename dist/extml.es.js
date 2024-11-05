40a57b49fa7bf4962f8156aebfed05d44569a920
fabioricali <f.ricali@xdevel.com>
2024-11-05 17:34:45 +0100

    BUILD

--- dist/extml.es.js
+++ dist/extml.es.js
@@ -1,4 +1,4 @@
-/* Extml, version: 2.4.1 - November 5, 2024 17:34:15 */
+/* Extml, version: 2.4.2 - November 5, 2024 17:34:44 */
 const STYLE_PREFIX = 'extml-style-';
 
 function composeStyleInner(cssContent, tag) {
--- dist/extml.umd.js
+++ dist/extml.umd.js
@@ -1,4 +1,4 @@
-/* Extml, version: 2.4.1 - November 5, 2024 17:34:15 */
+/* Extml, version: 2.4.2 - November 5, 2024 17:34:44 */
 (function(g,f){typeof exports==='object'&&typeof module!=='undefined'?f(exports):typeof define==='function'&&define.amd?define(['exports'],f):(g=typeof globalThis!=='undefined'?globalThis:g||self,f(g.extml={}));})(this,(function(exports){'use strict';const STYLE_PREFIX = 'extml-style-';
 
 function composeStyleInner(cssContent, tag) {
