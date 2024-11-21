c2194b184553c10cb4115c1ebd9388a022a18259
fabioricali <f.ricali@xdevel.com>
2024-11-21 17:03:55 +0100

    progress For component

--- dist/extml.es.js
+++ dist/extml.es.js
@@ -1,4 +1,4 @@
-/* Extml, version: 2.6.12 - November 21, 2024 08:40:54 */
+/* Extml, version: 2.6.12 - November 21, 2024 17:00:54 */
 const STYLE_PREFIX = 'extml-style-';
 
 function composeStyleInner(cssContent, tag) {
@@ -143,7 +143,7 @@ function destroy() {
 }const htmlTags = [
     "a", "abbr", "acronym", "address", "applet", "area", "article", "aside", "audio", "b", "base", "basefont", "bdi", "bdo", "bgsound", "big", "blink", "blockquote", "body", "br", "button", "canvas", "caption", "center", "cite", "code", "col", "colgroup", "command", "content", "data", "datalist", "dd", "del", "details", "dfn", "dialog", "dir", "div", "dl", "dt", "element", "em", "embed", "fieldset", "figcaption", "figure", "font", "footer", "form", "frame", "frameset", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hgroup", "hr", "html", "i", "iframe", "image", "img", "input", "ins", "isindex", "kbd", "keygen", "label", "legend", "li", "link", "listing", "main", "map", "mark", "marquee", "math", "menu", "menuitem", "meta", "meter", "multicol", "nav", "nextid", "nobr", "noembed", "noframes", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "plaintext", "pre", "progress", "q", "rb", "rbc", "rp", "rt", "rtc", "ruby", "s", "samp", "script", "section", "select", "shadow", "slot", "small", "source", "spacer", "span", "strike", "strong", "style", "sub", "summary", "sup", "svg", "table", "tbody", "td", "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "tt", "u", "ul", "var", "video", "wbr", "xmp",
     //custom tag
-    "x-state", "x-html"
+    "x-state", "x-html", "x-for"
 ];function defineExtClass(tag) {
     let className = 'html-' + tag;
     window.__extHtmlClass[className] = window.Ext.define(className, {
@@ -1034,15 +1034,82 @@ function createRef(onChange) {
     });
 
     return derived;
-}function For({each, func}) {
+}function For({ each, func }) {
     function onInitialize(component) {
-        console.log(each);
-        console.log(func);
-        component.add(func());
+        let currentItems = [];
+
+        const updateChildren = (newItems) => {
+            newItems.forEach((item, index) => {
+                if (!currentItems[index] || !deepEqual(currentItems[index], item)) {
+                    const child = func(item, index);
+                    if (component.items.getAt(index)) {
+                        component.removeAt(index);
+                        component.insert(index, child);
+                    } else {
+                        component.add(child);
+                    }
+                }
+            });
+
+            while (component.items.length > newItems.length) {
+                component.removeAt(newItems.length);
+            }
+
+            currentItems = newItems;
+        };
+
+        if (Array.isArray(each)) {
+            updateChildren(each);
+        } else {
+            updateChildren(each());
+            if (each.$$isState) {
+                each.$$subscribe(updateChildren);
+            }
+        }
     }
 
-    return h`<ext-container oninitialize="${onInitialize}"></ext-container>`
-}try {
+    return h`<ext-container oninitialize="${onInitialize}"></ext-container>`;
+}
+
+// Utility function for deep comparison
+function deepEqual(a, b) {
+    if (a === b) return true;
+
+    if (typeof a === 'object' && typeof b === 'object' && a !== null && b !== null) {
+        const keysA = Object.keys(a);
+        const keysB = Object.keys(b);
+
+        if (keysA.length !== keysB.length) return false;
+
+        return keysA.every(key => deepEqual(a[key], b[key]));
+    }
+
+    return false;
+}
+
+// export function For({ each, func }) {
+//     function onInitialize(component) {
+//         // Aggiunge dinamicamente i figli generati da func() in base a `each`
+//         each().forEach((item, index) => {
+//             const child = func(item, index); // Genera il contenuto per ogni elemento
+//             component.add(child);
+//         });
+//
+//         // Iscrivi un listener per aggiornare i contenuti se `each` è uno stato reattivo
+//         if (each.$$isState) {
+//             each.$$subscribe((newItems) => {
+//                 component.removeAll(); // Rimuove i componenti precedenti
+//                 newItems.forEach((item, index) => {
+//                     const child = func(item, index);
+//                     component.add(child);
+//                 });
+//             });
+//         }
+//     }
+//
+//     return h`<ext-container oninitialize="${onInitialize}"></ext-container>`;
+// }
+try {
     if (window) {
         generateHtmlClass();
     }
--- src/For.js
+++ src/For.js
@@ -1,11 +1,77 @@
 import {h} from "./parser.js";
 
-export function For({each, func}) {
+export function For({ each, func }) {
     function onInitialize(component) {
-        console.log(each);
-        console.log(func)
-        component.add(func())
+        let currentItems = [];
+
+        const updateChildren = (newItems) => {
+            newItems.forEach((item, index) => {
+                if (!currentItems[index] || !deepEqual(currentItems[index], item)) {
+                    const child = func(item, index);
+                    if (component.items.getAt(index)) {
+                        component.removeAt(index);
+                        component.insert(index, child);
+                    } else {
+                        component.add(child);
+                    }
+                }
+            });
+
+            while (component.items.length > newItems.length) {
+                component.removeAt(newItems.length);
+            }
+
+            currentItems = newItems;
+        };
+
+        if (Array.isArray(each)) {
+            updateChildren(each);
+        } else {
+            updateChildren(each());
+            if (each.$$isState) {
+                each.$$subscribe(updateChildren);
+            }
+        }
     }
 
-    return h`<ext-container oninitialize="${onInitialize}"></ext-container>`
+    return h`<ext-container oninitialize="${onInitialize}"></ext-container>`;
 }
+
+// Utility function for deep comparison
+function deepEqual(a, b) {
+    if (a === b) return true;
+
+    if (typeof a === 'object' && typeof b === 'object' && a !== null && b !== null) {
+        const keysA = Object.keys(a);
+        const keysB = Object.keys(b);
+
+        if (keysA.length !== keysB.length) return false;
+
+        return keysA.every(key => deepEqual(a[key], b[key]));
+    }
+
+    return false;
+}
+
+// export function For({ each, func }) {
+//     function onInitialize(component) {
+//         // Aggiunge dinamicamente i figli generati da func() in base a `each`
+//         each().forEach((item, index) => {
+//             const child = func(item, index); // Genera il contenuto per ogni elemento
+//             component.add(child);
+//         });
+//
+//         // Iscrivi un listener per aggiornare i contenuti se `each` è uno stato reattivo
+//         if (each.$$isState) {
+//             each.$$subscribe((newItems) => {
+//                 component.removeAll(); // Rimuove i componenti precedenti
+//                 newItems.forEach((item, index) => {
+//                     const child = func(item, index);
+//                     component.add(child);
+//                 });
+//             });
+//         }
+//     }
+//
+//     return h`<ext-container oninitialize="${onInitialize}"></ext-container>`;
+// }
