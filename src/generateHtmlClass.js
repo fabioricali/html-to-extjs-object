7b18b0966c1009ae32d5f0e416fdadccede33d8f
fabioricali <f.ricali@xdevel.com>
2024-11-23 11:48:54 +0100

    remove original attributes from html extjs element

--- src/generateHtmlClass.js
+++ src/generateHtmlClass.js
@@ -20,108 +20,107 @@ export function defineExtClass(tag) {
         getInnerHtmlElement() {
             return this.getRenderTarget();
         },
-        __initializeHackAttempts: 0,
-        __initializeHack(o) {
-            {
-                if (this.__initializeHackAttempts > 2) {
-                    // console.warn('this.el.dom not found', this)
-                    return;
-                }
-                if (!this.el || !this.el.dom ) {
-                    this.__initializeHackAttempts++;
-                    requestAnimationFrame(() => this.__initializeHack(o));
-                    return
+        initialize() {
+            this.removeMonitorElement();
+            this.moveChildNodesToNewParent();
+            requestAnimationFrame(this.initReactivity.bind(this));
+        },
+        removeMonitorElement() {
+            // Rimuovo i due div del monitor, nodi dovrebbero essere sempre 3, quindi vado a colpo sicuro,
+            // il primo è il bodyElement, quello che poi conterrà eventuali figli
+            let sizeMonitorsEl = this.el.dom.childNodes[1];
+            let paintMonitorEl = this.el.dom.childNodes[2];
+
+            if (sizeMonitorsEl)
+                sizeMonitorsEl.remove();
+            if (paintMonitorEl)
+                paintMonitorEl.remove();
+        },
+        moveChildNodesToNewParent() {
+            // sposto gli elementi dal ghost-tag al parent
+            let newParent = this.el.dom;
+            let oldParent = this.innerElement.dom;
+
+            if (newParent && oldParent) {
+                function move() {
+                    while (oldParent.childNodes.length > 0) {
+                        newParent.appendChild(oldParent.childNodes[0]);
+                    }
                 }
 
-                this.el.dom.className = '';
-                this.innerElement.dom.className = '';
-                if (this._propsAttributes) {
-                    Object.keys(this._propsAttributes).forEach(attribute => {
-                        if (attribute === 'ref' && this._propsAttributes[attribute].$$isRef) {
-                            this._propsAttributes[attribute](o.el.dom)
-                        } else if (this._propsAttributes[attribute].$$isState) {
-                            this.el.dom.setAttribute(attribute, String(this._propsAttributes[attribute]()));
-                            o.$$stateListener = this._propsAttributes[attribute].$$subscribe(value => {
-                                this.el.dom.setAttribute(attribute, String(value));
+                move();
+
+                this.innerElement.dom = this.el.dom;
+
+                // Rimuovo ghost-tag
+                oldParent.remove();
+            }
+        },
+        removeOriginalAttributes() {
+            this.el.dom.removeAttribute('class');
+            // l'id mi serve per lo style
+            if (!this.stylesheet)
+                this.el.dom.removeAttribute('id');
+            this.el.dom.removeAttribute('data-componentid');
+            this.el.dom.removeAttribute('data-xid');
+        },
+        initReactivity() {
+            if (!this.el || !this.el.dom) {
+                return
+            }
+
+            this.removeOriginalAttributes();
+
+            if (this._propsAttributes) {
+                Object.keys(this._propsAttributes).forEach(attribute => {
+                    if (attribute === 'ref' && this._propsAttributes[attribute].$$isRef) {
+                        this._propsAttributes[attribute](this.el.dom)
+                    } else if (this._propsAttributes[attribute].$$isState) {
+                        this.el.dom.setAttribute(attribute, String(this._propsAttributes[attribute]()));
+                        this.$$stateListener = this._propsAttributes[attribute].$$subscribe(value => {
+                            this.el.dom.setAttribute(attribute, String(value));
+                        })
+                    } else if (attribute.startsWith('on') && typeof this._propsAttributes[attribute] === 'function') {
+                        this.el.dom.addEventListener(attribute.substring(2), this._propsAttributes[attribute]);
+                    } else {
+                        if (Array.isArray(this._propsAttributes[attribute]) && this._propsAttributes[attribute].$$hasState) {
+                            // mi serve a costruire il valore dell'attributo concatenando i risultati degli state con le eventuali stringhe presenti
+                            let buildAttributeValue = () => this.el.dom.setAttribute(attribute, this._propsAttributes[attribute].map(item => {
+                                if (typeof item === 'function' && item.$$isState) {
+                                    return item()
+                                } else {
+                                    return item
+                                }
+                            }).join(''));
+
+                            buildAttributeValue();
+
+                            this.$$attributesStateListeners = [];
+                            // per ogni state sottoscrivo un listener per ricostruire nuovamente il valore dell'attributo ad ogni cambiamento
+                            this._propsAttributes[attribute].forEach(item => {
+                                if (typeof item === 'function' && item.$$isState) {
+                                    this.$$attributesStateListeners.push(item.$$subscribe(value => {
+                                        buildAttributeValue();
+                                    }))
+                                }
                             })
-                        } else if (attribute.startsWith('on') && typeof this._propsAttributes[attribute] === 'function') {
-                            this.el.dom.addEventListener(attribute.substring(2), this._propsAttributes[attribute]);
+
                         } else {
-                            if (Array.isArray(this._propsAttributes[attribute]) && this._propsAttributes[attribute].$$hasState) {
-                                //console.log(this._propsAttributes[attribute])
-                                // mi serve a costruire il valore dell'attributo concatenando i risultati degli state con le eventuali stringhe presenti
-                                let buildAttributeValue = () => this.el.dom.setAttribute(attribute, this._propsAttributes[attribute].map(item => {
-                                    if (typeof item === 'function' && item.$$isState) {
-                                        return item()
-                                    } else {
-                                        return item
-                                    }
-                                }).join(''));
-
-                                buildAttributeValue();
-
-                                o.$$attributesStateListeners = [];
-                                // per ogni state sottoscrivo un listener per ricostruire nuovamente il valore dell'attributo ad ogni cambiamento
-                                this._propsAttributes[attribute].forEach(item => {
-                                    if (typeof item === 'function' && item.$$isState) {
-                                        o.$$attributesStateListeners.push(item.$$subscribe(value => {
-                                            buildAttributeValue();
-                                        }))
-                                    }
-                                })
-
-                            } else {
-                                this.el.dom.setAttribute(attribute, String(this._propsAttributes[attribute]));
-                            }
+                            this.el.dom.setAttribute(attribute, String(this._propsAttributes[attribute]));
                         }
-                    });
-                }
+                    }
+                });
             }
         },
         listeners: [
             {
-                initialize(o) {
-                    //hack
-                    requestAnimationFrame(() => this.__initializeHack(o));
-
-                    // Rimuovo i due div del monitor, nodi dovrebbero essere sempre 3, quindi vado a colpo sicuro,
-                    // il primo è il bodyElement, quello che poi conterrà eventuali figli
-                    let sizeMonitorsEl = this.el.dom.childNodes[1];
-                    let paintMonitorEl = this.el.dom.childNodes[2];
-
-                    if (sizeMonitorsEl)
-                        sizeMonitorsEl.remove();
-                    if (paintMonitorEl)
-                        paintMonitorEl.remove();
-
-                    // sposto gli elementi dal ghost-tag al parent
-                    let newParent = this.el.dom;
-                    let oldParent = this.innerElement.dom;
-
-                    if (newParent && oldParent) {
-                        function move() {
-                            while (oldParent.childNodes.length > 0) {
-                                newParent.appendChild(oldParent.childNodes[0]);
-                            }
-                        }
-
-                        move();
-
-                        this.innerElement.dom = this.el.dom;
-
-                        // Rimuovo ghost-tag
-                        oldParent.remove();
-                    }
-                }
-            },
-            {
-                destroy(o) {
-                    if(o.$$stateListener) {
-                        o.$$stateListener()
+                destroy() {
+                    if (this.$$stateListener) {
+                        this.$$stateListener()
                     }
 
-                    if (o.$$attributesStateListeners) {
-                        o.$$attributesStateListeners.forEach(listener => listener());
+                    if (this.$$attributesStateListeners) {
+                        this.$$attributesStateListeners.forEach(listener => listener());
                     }
                 }
             }
@@ -129,7 +128,7 @@ export function defineExtClass(tag) {
     });
 }
 
-export function generateHtmlClass () {
+export function generateHtmlClass() {
     if (!window.__extHtmlClass) {
         window.__extHtmlClass = {};
         htmlTags.forEach(tag => defineExtClass(tag));
