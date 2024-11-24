028b582db011567f465feea33dcb7cc1117d2e1b
fabioricali <f.ricali@xdevel.com>
2024-11-24 19:55:16 +0100

    refactor

--- dist/extml.umd.js
+++ dist/extml.umd.js
@@ -1,4 +1,4 @@
-/* Extml, version: 2.26.0 - November 24, 2024 19:37:06 */
+/* Extml, version: 2.26.0 - November 24, 2024 19:52:40 */
 (function(g,f){typeof exports==='object'&&typeof module!=='undefined'?f(exports):typeof define==='function'&&define.amd?define(['exports'],f):(g=typeof globalThis!=='undefined'?globalThis:g||self,f(g.extml={}));})(this,(function(exports){'use strict';const STYLE_PREFIX = 'extml-style-';
 
 function composeStyleInner(cssContent, tag) {
@@ -164,7 +164,7 @@ function destroy() {
         getInnerHtmlElement() {
             return this.getRenderTarget();
         },
-        __initializeHack(o) {
+        doHack() {
             {
                 if (!this.el || !this.el.dom ) {
                     return
@@ -261,9 +261,8 @@ function destroy() {
         },
         listeners: [
             {
-                initialize(o) {
-                    //hack
-                    requestAnimationFrame(() => this.__initializeHack(o));
+                initialize() {
+                    requestAnimationFrame(() => this.doHack());
                     this.removeMonitorsElements();
                     this.moveElementsToNewParent();
                 }
